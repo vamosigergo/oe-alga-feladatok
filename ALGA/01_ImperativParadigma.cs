1aac06e5ac071dd84700a85b83090d9ead4c3f3a
Gergő Vámosi <vamosi.gergo04@gmail.com>
2024-09-14 13:35:53 +0200

    1. heti feladat

---
+++ ALGA/01_ImperativParadigma.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.ALGA.Paradigmak
+{
+    public interface IVegrehajthato
+    {
+        void Vegrehajtas();
+    }
+
+    public interface IFuggo
+    {
+        bool FuggosegTeljesul { get; }
+    }
+
+
+    public class FeladatTarolo<T> : IEnumerable<T> where T : IVegrehajthato
+    {
+        public T[] tarolo;
+        public int n = 0;
+        public FeladatTarolo(int meret)
+        {
+            tarolo = new T[meret];
+            n++;
+
+        }
+
+        public void Felvesz(T elem)
+        {
+            if (n < tarolo.Length)
+            {
+                tarolo[n] = elem;
+                n++;
+            }
+            else
+            {
+                throw new TaroloMegteltKivetel();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void MindentVegrehajt()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                tarolo[i].Vegrehajtas();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            TaroloBejaro<T> bejaro = new TaroloBejaro<T>(tarolo, n);
+            return bejaro;
+        }
+    }
+
+    public class FuggoFeladatTarolo<T> : FeladatTarolo<T> where T : IVegrehajthato, IFuggo
+    {
+        public FuggoFeladatTarolo(int meret) : base(meret)
+        {
+        }
+
+        public override void MindentVegrehajt()
+        {
+            for (int i = 0; i < n; i++)
+            {
+
+                if (tarolo[i].FuggosegTeljesul)
+                {
+                    tarolo[i].Vegrehajtas();
+                }
+            }
+        }
+    }
+
+    public class TaroloBejaro<T> : IEnumerator<T>
+    {
+        T[] E;
+        int n;
+        int AktualisIndex = -1;
+
+        public TaroloBejaro(T[] E, int n)
+        {
+            this.E = E;
+            this.n = n;
+        }
+
+        public T Current
+        {
+
+            get { return E[AktualisIndex]; } 
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            if(AktualisIndex < n-1)
+            {
+                AktualisIndex++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            AktualisIndex = -1;
+        }
+    }
+
+    
+
+    public class TaroloMegteltKivetel() : Exception
+    {
+
+    }
+
+
+
+
+
+
+}
--- Tesztek/01_ImperativParadigmaTesztek.cs
+++ Tesztek/01_ImperativParadigmaTesztek.cs
@@ -1,257 +1,257 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using OE.ALGA.Paradigmak;
-// 
-// namespace OE.ALGA.Tesztek
-// {
-//     class TesztFeladat : IVegrehajthato //F1.
-//     {
-//         public string Azonosito { get; set; }
-// 
-//         public bool Vegrehajtott { get; set; }
-// 
-//         public void Vegrehajtas()
-//         {
-//             Vegrehajtott = true;
-//         }
-// 
-//         public TesztFeladat(string nev)
-//         {
-//             this.Azonosito = nev;
-//         }
-//     }
-// 
-//     [TestClass()]
-//     public class FeladatTaroloTeszt
-//     {
-//         [TestMethod()]
-//         public void FelveszTeszt() //F2.(c)
-//         {
-//             FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(5);
-//             TesztFeladat a = new TesztFeladat("a");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//         }
-// 
-//         [TestMethod()]
-//         [ExpectedException(typeof(TaroloMegteltKivetel))]
-//         public void TulsokatFelveszTeszt() //F2.(c)
-//         {
-//             FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(5);
-//             TesztFeladat a = new TesztFeladat("a");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//         }
-// 
-//         [TestMethod()]
-//         public void VegrehajtasTeszt() //F2.(d)
-//         {
-//             FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(10);
-//             TesztFeladat a = new TesztFeladat("a");
-//             TesztFeladat b = new TesztFeladat("b");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(b);
-//             Assert.IsFalse(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott);
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsTrue(b.Vegrehajtott);
-//         }
-//     }
-// 
-//     class TesztFuggoFeladat : TesztFeladat, IFuggo //F3.
-//     {
-//         public bool Vegrehajthato { get; set; }
-// 
-//         public virtual bool FuggosegTeljesul
-//         {
-//             get
-//             {
-//                 return Vegrehajthato;
-//             }
-//         }
-// 
-//         public TesztFuggoFeladat(string nev) : base(nev)
-//         {
-//         }
-//     }
-// 
-//     [TestClass()]
-//     public class FuggoFeladatTaroloTeszt //F4.
-//     {
-//         [TestMethod()]
-//         public void FelveszTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//         }
-// 
-//         [TestMethod()]
-//         [ExpectedException(typeof(TaroloMegteltKivetel))]
-//         public void TulsokatFelveszTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(a);
-//         }
-// 
-//         [TestMethod()]
-//         public void VegrehajtasAmikorNemVegrehajthatokTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a");
-//             TesztFuggoFeladat b = new TesztFuggoFeladat("b");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(b);
-//             Assert.IsFalse(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott);
-//             tarolo.MindentVegrehajt();
-//             Assert.IsFalse(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott);
-//         }
-// 
-//         [TestMethod()]
-//         public void VegrehajtasAmikorVegrehajthatokTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a") { Vegrehajthato = true };
-//             TesztFuggoFeladat b = new TesztFuggoFeladat("b");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(b);
-//             Assert.IsFalse(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott);
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott);
-//             b.Vegrehajthato = true;
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsTrue(b.Vegrehajtott);
-//         }
-//     }
-// 
-//     class TesztElokovetelmenytolFuggoFeladat : TesztFuggoFeladat //F4.
-//     {
-//         readonly TesztFeladat elokovetelmeny;
-// 
-//         public TesztElokovetelmenytolFuggoFeladat(string nev, TesztFeladat elokovetelmeny) : base(nev)
-//         {
-//             this.elokovetelmeny = elokovetelmeny;
-//         }
-// 
-//         public override bool FuggosegTeljesul
-//         {
-//             get
-//             {
-//                 return base.FuggosegTeljesul && elokovetelmeny.Vegrehajtott;
-//             }
-//         }
-//     }
-// 
-//     [TestClass()]
-//     public class FuggoFeladatTaroloElokovetelmenyekkelTeszt //F4.
-//     {
-//         [TestMethod()]
-//         public void ElokovetelmenyesTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a");
-//             TesztElokovetelmenytolFuggoFeladat b = new TesztElokovetelmenytolFuggoFeladat("b", a) { Vegrehajthato = true };
-//             // a->b
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(b);
-//             tarolo.MindentVegrehajt();
-//             Assert.IsFalse(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott);
-//             a.Vegrehajthato = true;
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsTrue(b.Vegrehajtott);
-//         }
-// 
-//         [TestMethod()]
-//         public void TobbKorosElokovetelmenyesTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a") { Vegrehajthato = true };
-//             TesztElokovetelmenytolFuggoFeladat b = new TesztElokovetelmenytolFuggoFeladat("b", a) { Vegrehajthato = true };
-//             TesztElokovetelmenytolFuggoFeladat c = new TesztElokovetelmenytolFuggoFeladat("c", a) { Vegrehajthato = true };
-//             TesztElokovetelmenytolFuggoFeladat d = new TesztElokovetelmenytolFuggoFeladat("d", b) { Vegrehajthato = true };
-//             // a->b->d
-//             //  ->c
-//             tarolo.Felvesz(d);
-//             tarolo.Felvesz(c);
-//             tarolo.Felvesz(b);
-//             tarolo.Felvesz(a);
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsFalse(b.Vegrehajtott); // a 'b' kiértékelése az 'a' végrehajtása előtt volt, ezért az még nem lett feldolgozva
-//             Assert.IsFalse(c.Vegrehajtott); // a 'c' kiértékelése az 'a' végrehajtása előtt volt, ezért az még nem lett feldolgozva
-//             Assert.IsFalse(d.Vegrehajtott);
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsTrue(b.Vegrehajtott); 
-//             Assert.IsTrue(c.Vegrehajtott);
-//             Assert.IsFalse(d.Vegrehajtott); // a 'd' kiértékelése a 'b' végrehajtása előtt volt, ezért az még nem lett feldolgozva
-//             tarolo.MindentVegrehajt();
-//             Assert.IsTrue(a.Vegrehajtott);
-//             Assert.IsTrue(b.Vegrehajtott);
-//             Assert.IsTrue(c.Vegrehajtott);
-//             Assert.IsTrue(d.Vegrehajtott);
-//         }
-//     }
-// 
-//     class BejarasokTeszt //F5.
-//     {
-//         [TestMethod()]
-//         public void FeladatTaroloBejaroTeszt()
-//         {
-//             FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(10);
-//             TesztFeladat a = new TesztFeladat("a");
-//             TesztFeladat b = new TesztFeladat("b");
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(b);
-//             string nevek = "";
-//             foreach (TesztFeladat u in tarolo)
-//             {
-//                 nevek += u.Azonosito;
-//             }
-//             Assert.AreEqual("ab", nevek);
-//         }
-// 
-//         [TestMethod()]
-//         public void FuggoFeladatTaroloBejaroTeszt()
-//         {
-//             FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
-//             TesztFuggoFeladat a = new TesztFuggoFeladat("a");
-//             TesztFuggoFeladat b = new TesztFuggoFeladat("b") { Vegrehajthato = true };
-//             tarolo.Felvesz(a);
-//             tarolo.Felvesz(b);
-//             tarolo.MindentVegrehajt();
-//             string nevek = "";
-//             foreach (TesztFuggoFeladat u in tarolo)
-//             {
-//                 if (u.Vegrehajtott)
-//                     nevek += u.Azonosito;
-//             }
-//             Assert.AreEqual("b", nevek);
-//         }
-// 
-//     }
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OE.ALGA.Paradigmak;
+
+namespace OE.ALGA.Tesztek
+{
+    class TesztFeladat : IVegrehajthato //F1.
+    {
+        public string Azonosito { get; set; }
+
+        public bool Vegrehajtott { get; set; }
+
+        public void Vegrehajtas()
+        {
+            Vegrehajtott = true;
+        }
+
+        public TesztFeladat(string nev)
+        {
+            this.Azonosito = nev;
+        }
+    }
+
+    [TestClass()]
+    public class FeladatTaroloTeszt
+    {
+        [TestMethod()]
+        public void FelveszTeszt() //F2.(c)
+        {
+            FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(5);
+            TesztFeladat a = new TesztFeladat("a");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(TaroloMegteltKivetel))]
+        public void TulsokatFelveszTeszt() //F2.(c)
+        {
+            FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(5);
+            TesztFeladat a = new TesztFeladat("a");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+        }
+
+        [TestMethod()]
+        public void VegrehajtasTeszt() //F2.(d)
+        {
+            FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(10);
+            TesztFeladat a = new TesztFeladat("a");
+            TesztFeladat b = new TesztFeladat("b");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(b);
+            Assert.IsFalse(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott);
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsTrue(b.Vegrehajtott);
+        }
+    }
+
+    class TesztFuggoFeladat : TesztFeladat, IFuggo //F3.
+    {
+        public bool Vegrehajthato { get; set; }
+
+        public virtual bool FuggosegTeljesul
+        {
+            get
+            {
+                return Vegrehajthato;
+            }
+        }
+
+        public TesztFuggoFeladat(string nev) : base(nev)
+        {
+        }
+    }
+
+    [TestClass()]
+    public class FuggoFeladatTaroloTeszt //F4.
+    {
+        [TestMethod()]
+        public void FelveszTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(TaroloMegteltKivetel))]
+        public void TulsokatFelveszTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(a);
+        }
+
+        [TestMethod()]
+        public void VegrehajtasAmikorNemVegrehajthatokTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a");
+            TesztFuggoFeladat b = new TesztFuggoFeladat("b");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(b);
+            Assert.IsFalse(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott);
+            tarolo.MindentVegrehajt();
+            Assert.IsFalse(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott);
+        }
+
+        [TestMethod()]
+        public void VegrehajtasAmikorVegrehajthatokTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a") { Vegrehajthato = true };
+            TesztFuggoFeladat b = new TesztFuggoFeladat("b");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(b);
+            Assert.IsFalse(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott);
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott);
+            b.Vegrehajthato = true;
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsTrue(b.Vegrehajtott);
+        }
+    }
+
+    class TesztElokovetelmenytolFuggoFeladat : TesztFuggoFeladat //F4.
+    {
+        readonly TesztFeladat elokovetelmeny;
+
+        public TesztElokovetelmenytolFuggoFeladat(string nev, TesztFeladat elokovetelmeny) : base(nev)
+        {
+            this.elokovetelmeny = elokovetelmeny;
+        }
+
+        public override bool FuggosegTeljesul
+        {
+            get
+            {
+                return base.FuggosegTeljesul && elokovetelmeny.Vegrehajtott;
+            }
+        }
+    }
+
+    [TestClass()]
+    public class FuggoFeladatTaroloElokovetelmenyekkelTeszt //F4.
+    {
+        [TestMethod()]
+        public void ElokovetelmenyesTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a");
+            TesztElokovetelmenytolFuggoFeladat b = new TesztElokovetelmenytolFuggoFeladat("b", a) { Vegrehajthato = true };
+            // a->b
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(b);
+            tarolo.MindentVegrehajt();
+            Assert.IsFalse(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott);
+            a.Vegrehajthato = true;
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsTrue(b.Vegrehajtott);
+        }
+
+        [TestMethod()]
+        public void TobbKorosElokovetelmenyesTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a") { Vegrehajthato = true };
+            TesztElokovetelmenytolFuggoFeladat b = new TesztElokovetelmenytolFuggoFeladat("b", a) { Vegrehajthato = true };
+            TesztElokovetelmenytolFuggoFeladat c = new TesztElokovetelmenytolFuggoFeladat("c", a) { Vegrehajthato = true };
+            TesztElokovetelmenytolFuggoFeladat d = new TesztElokovetelmenytolFuggoFeladat("d", b) { Vegrehajthato = true };
+            // a->b->d
+            //  ->c
+            tarolo.Felvesz(d);
+            tarolo.Felvesz(c);
+            tarolo.Felvesz(b);
+            tarolo.Felvesz(a);
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsFalse(b.Vegrehajtott); // a 'b' kiértékelése az 'a' végrehajtása előtt volt, ezért az még nem lett feldolgozva
+            Assert.IsFalse(c.Vegrehajtott); // a 'c' kiértékelése az 'a' végrehajtása előtt volt, ezért az még nem lett feldolgozva
+            Assert.IsFalse(d.Vegrehajtott);
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsTrue(b.Vegrehajtott);
+            Assert.IsTrue(c.Vegrehajtott);
+            Assert.IsFalse(d.Vegrehajtott); // a 'd' kiértékelése a 'b' végrehajtása előtt volt, ezért az még nem lett feldolgozva
+            tarolo.MindentVegrehajt();
+            Assert.IsTrue(a.Vegrehajtott);
+            Assert.IsTrue(b.Vegrehajtott);
+            Assert.IsTrue(c.Vegrehajtott);
+            Assert.IsTrue(d.Vegrehajtott);
+        }
+    }
+
+    class BejarasokTeszt //F5.
+    {
+        [TestMethod()]
+        public void FeladatTaroloBejaroTeszt()
+        {
+            FeladatTarolo<TesztFeladat> tarolo = new FeladatTarolo<TesztFeladat>(10);
+            TesztFeladat a = new TesztFeladat("a");
+            TesztFeladat b = new TesztFeladat("b");
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(b);
+            string nevek = "";
+            foreach (TesztFeladat u in tarolo)
+            {
+                nevek += u.Azonosito;
+            }
+            Assert.AreEqual("ab", nevek);
+        }
+
+        [TestMethod()]
+        public void FuggoFeladatTaroloBejaroTeszt()
+        {
+            FuggoFeladatTarolo<TesztFuggoFeladat> tarolo = new FuggoFeladatTarolo<TesztFuggoFeladat>(5);
+            TesztFuggoFeladat a = new TesztFuggoFeladat("a");
+            TesztFuggoFeladat b = new TesztFuggoFeladat("b") { Vegrehajthato = true };
+            tarolo.Felvesz(a);
+            tarolo.Felvesz(b);
+            tarolo.MindentVegrehajt();
+            string nevek = "";
+            foreach (TesztFuggoFeladat u in tarolo)
+            {
+                if (u.Vegrehajtott)
+                    nevek += u.Azonosito;
+            }
+            Assert.AreEqual("b", nevek);
+        }
+
+    }
+}
